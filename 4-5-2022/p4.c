379e72f8714916875cf438a58095650949a3274d
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-04 22:23:37 +0530

    Update p4.c

--- 4-5-2022/p4.c
+++ 4-5-2022/p4.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 #define SQUARE(x) ((x)*(x))
+#define MAX(x,y) ((x)>(y) ? (x):(y))
 #define MIN(x,y) ((x)<(y) ? (x):(y))
 #define ISLOWER(c) (c>=97 && c<=122)
+#define ISUPPER(c) (c>=65 && c<97)
 int main()
 {
-    int k=4,j=5,s;
-    s=SQUARE(k++);
-    char a,b,z;
-    a=b;
-    z = ISLOWER(a);
-    printf("s is lower %c",s);
+    int k=4,j=5,s,p,q,r,e;
+    char a,b,z,A;
+    e=SQUARE(k++);
+    p=MAX(j,k);
+    q=MIN(j,k);
+    a = b;
+    r = ISLOWER(a);
+    z=A;
+    s = ISUPPER(z);
+    printf("e=%d,p=%d, q=%d,r=%c,s=%c",e,p,q,r,s);
 }
