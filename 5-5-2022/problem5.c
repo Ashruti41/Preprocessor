2044775de8e8fea7f079159efc289a772fb42741
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-05 14:59:17 +0530

    Create problem5.c

---
+++ 5-5-2022/problem5.c
@@ -0,0 +1,23 @@
+//mecros that be used to generate different functions for different data types which called generic function.
+//in this program we create one generic function fo maximum
+
+#include <stdio.h>
+#define MAX(FNAME,DTYPE) \                                   
+DTYPE FNAME(DTYPE X, DTYPE Y) \                                
+{                         \
+    return X>Y ? X:Y;  \
+}
+MAX(max_int, int)
+MAX(max_float, float)
+MAX(max_double, double)
+
+int main()
+{
+    int p;
+    float q;
+    double r;
+    p=max_int(3,9);
+    q=max_float(7.4,5.7);
+    r=max_double(12.34,13.54);
+    printf("p=%d,q=%.2f,r=%.2lf \n",p,q,r);
+}
