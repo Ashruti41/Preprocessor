3652059eb5a76012df634d0f0f47a6b89da87b14
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-05 17:58:31 +0530

    Create problem13.c

---
+++ 5-5-2022/problem13.c
@@ -0,0 +1,29 @@
+//#if..#elif..#else..#endif    we can take nested loop of if in our macro code
+
+#include<stdio.h>
+#define FLAG 1
+
+int main()
+{
+    int a=20, b=4;
+    #if FLAG==0
+    printf("Value of FLAG is zero\n");
+    a++;
+    b++;
+    #elif FLAG==1
+    printf("Value of FLAG is One\n");
+    a--;
+    b--;
+    #elif FLAG==2
+    printf("Value of FLAG is two");
+    a=a-3;
+    b=b-3;
+    #else
+    printf("Value of FLAG is greater than two or less than zero\n");
+    a=a+b;
+    b=a-b;
+    #endif
+    
+    printf("a = %d,b=%d\n",a,b);
+    printf("Program completed");
+}
