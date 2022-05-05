d772dd58094677dd34a8bbf5e6fa76af8880f775
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-05 12:14:11 +0530

    Update problem2.c

--- 5-5-2022/problem2.c
+++ 5-5-2022/problem2.c
@@ -8,3 +8,16 @@ int main()
     printf("PROD is %d\n",PROD(2,3));
     printf("PROD1 is %d",PROD(2+1,3+4));
 }
+
+
+
+//with parentheses
+#include<stdio.h>
+#define PROD(x,y) (x)*(y)
+
+int main()
+{
+    printf("PROD is %d\n",PROD(2,3));
+    printf("PROD1 is %d\n",PROD(2+1,3+4));
+    printf("Prod2 is %d",60/PROD(1,4));
+}
