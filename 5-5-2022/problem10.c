c787c69458898400246226485b8854e1a6fe8166
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-05 17:40:50 +0530

    Create problem10.c

---
+++ 5-5-2022/problem10.c
@@ -0,0 +1,14 @@
+//#if....#endif when we take macro value as 0 or an expression which is followed by #if is evaluated, if result is non-zero then the statements between #if and #endif are compiled otherwise they are skipped
+
+
+#include<stdio.h>
+#define Flag 0                                       //here flag=0 so #if..#endif statement will skipped and direct gives output as hi
+                                                    //so when we take Flag>0 then and then hello message will print
+
+int main()
+{
+    #if Flag  
+    printf("hello");
+    #endif
+    printf("hi");
+}
