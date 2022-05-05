709e33590a2098f96cdbe2e9f78032fbc35c3e22
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-05 15:15:04 +0530

    Create problem6.c

---
+++ 5-5-2022/problem6.c
@@ -0,0 +1,17 @@
+//#define is used for define a name as global
+//where as #undef is used for undefined the macro.
+// when we use first #define and then #undef that time #undef is responsible for undefined the macors in the program
+
+#include<stdio.h>
+#define PI 
+ 
+int main()
+{
+    #undef PI
+    #if defined(PI)
+    printf("macro has been defined");
+    #else
+    printf("macro has been undefined");
+    #endif
+    
+}
