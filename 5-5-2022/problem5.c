61c01ecd8093b7ce60c40e4ccc3f1d5b2281337f
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 12:11:20 +0530

    Update problem5.c

--- 5-5-2022/problem5.c
+++ 5-5-2022/problem5.c
@@ -2,7 +2,7 @@
 //in this program we create one generic function fo maximum
 
 #include <stdio.h>
-#define MAX(FNAME,DTYPE) \                                   
+#define MAX(FNAME,DTYPE) \                                    //this is the macro function for different data types          
 DTYPE FNAME(DTYPE X, DTYPE Y) \                                
 {                         \
     return X>Y ? X:Y;  \
@@ -16,8 +16,13 @@ int main()
     int p;
     float q;
     double r;
-    p=max_int(3,9);
+    p=max_int(3,9);                                        //check in int format for maximum number
     q=max_float(7.4,5.7);
     r=max_double(12.34,13.54);
     printf("p=%d,q=%.2f,r=%.2lf \n",p,q,r);
 }
+
+
+/*OUTPUT:
+p=9,q=7.4,r=13.54
+*/
