4c836475b853cdd2f5577d0f79d50bc0476a582a
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 11:58:21 +0530

    Update problem2.c

--- 5-5-2022/problem2.c
+++ 5-5-2022/problem2.c
@@ -8,10 +8,13 @@ int main()
     printf("PROD is %d\n",PROD(2,3));
     printf("PROD1 is %d",PROD(2+1,3+4));
 }
+/*OUTPUT:
+PROD is 6
+PROD1 is 9
+*/
 
 
-
-//with parentheses
+//with parentheses ans will be more accurate as compare to program without any parentheses
 #include<stdio.h>
 #define PROD(x,y) (x)*(y)
 
@@ -21,3 +24,9 @@ int main()
     printf("PROD1 is %d\n",PROD(2+1,3+4));
     printf("Prod2 is %d",60/PROD(1,4));
 }
+
+/*OUTPUT:
+PROD is 6
+PROD1 is 21
+PROD2 is 240
+*/
