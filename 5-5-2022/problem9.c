6170754af6755a059bacf94b72c826a8dc755be6
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 12:19:31 +0530

    Update problem9.c

--- 5-5-2022/problem9.c
+++ 5-5-2022/problem9.c
@@ -8,6 +8,11 @@ int main()
 {
     int k2=14, k3=25;
     int marks_chem =89, marks_maths=98;
-    printf("%d %d\n",PASTE(k,2),PASTE(k,3));
+    printf("%d %d\n",PASTE(k,2),PASTE(k,3));                    
     printf("%d %d\n",MARKS(chem),MARKS(maths));
 }
+
+/*OUTPUT:
+14 25
+89 98
+*/
