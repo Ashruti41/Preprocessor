1fe6efd1528299b1367cf2a72934adbf9dd2b29f
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 11:40:06 +0530

    Update p2.c

--- 6-6-2022/p2.c
+++ 6-6-2022/p2.c
@@ -6,7 +6,7 @@
 int main()
 {
     int a=20,b=4;
-    #ifndef MAX
+    #ifndef MAX                                      //here MAX is not defined earlier for use of #ifndef we can directly run our code without define macros
     printf("MAX is not defined\n");
     a--;
     b--;
@@ -14,3 +14,10 @@ int main()
     printf("a=%d, b=%d\n",a,b);
     printf("Program completed\n");
 }
+
+
+/*  OUTPUT:
+MAX is not defined
+a=19, b=3
+Program completed
+*/
