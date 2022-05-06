7e212b1582a985209054307ad31d26ee421218e0
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 10:10:52 +0530

    Create p1.c

---
+++ 6-6-2022/p1.c
@@ -0,0 +1,22 @@
+//use of #undef and #ifdef directives
+
+
+#include <stdio.h>
+#define FLAG
+int main()
+{
+    int a=20,b=4;
+    #ifdef FLAG
+    printf("FLAG is defined\n");
+    a++;
+    b++;
+    #endif
+    #undef FLAG
+    #ifdef FLAG
+    printf("Prepocessor\n");
+    a++;
+    b++;
+    #endif
+    printf("a=%d, b=%d\n",a,b);
+    printf("Program completed\n");
+}
