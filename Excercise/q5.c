0ec61a2ea5ee5db7d4607516df39a206b7899db0
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 14:20:59 +0530

    Create q5.c

---
+++ Excercise/q5.c
@@ -0,0 +1,24 @@
+//Q5.
+
+#include<stdio.h>
+#define NEW_LINE printf("\n");
+#define BLANK_LINES(n) {int i; for(i=0;i<n;i++) printf("\n");}
+
+int main()
+{
+    printf("When you have a chance");
+    NEW_LINE
+    printf("TO EMBRACE AN OPPORTUNITY");
+    BLANK_LINES(3)
+    printf("Give it a big hug");
+    NEW_LINE
+}
+
+/*OUTPUT:
+When you have a chance
+TO EMBRACE AN OPPORTUNITY
+
+
+Give it a big hug
+*/
+
