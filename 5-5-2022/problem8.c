fa722355453de0b05a46b5f25720712ca4c598dc
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 12:17:35 +0530

    Update problem8.c

--- 5-5-2022/problem8.c
+++ 5-5-2022/problem8.c
@@ -1,4 +1,5 @@
 //stringizing operator
+//it causes the argument it precedes to be turned into string 
 
 #include<stdio.h>
 #define SHOW(var,format) printf(#var "=% " #format "\n",var);
@@ -7,8 +8,15 @@ int main()
     int x=9;
     float y=2.5;
     char z='$';
-    SHOW(x,d);
+    SHOW(x,d);                      //here x is int and d for int format
     SHOW(y,0.2f);
     SHOW(z,c);
     SHOW(x*y,0.2f);
 }
+
+/*OUTPUT:
+x= 9
+y= 2.50
+z=$
+x*y= 22.50
+*/
