b47c3247935f066dc9eab5a2608eb2fb331a51d5
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 11:50:27 +0530

    Update p2.c

--- 4-5-2022/p2.c
+++ 4-5-2022/p2.c
@@ -7,11 +7,16 @@ int main()
 {
     int l,m,i,j,a=5,b=3;
     float p,q;
-    l=SUM(a,b);
-    m=PROD(a,b);
+    l=SUM(a,b);                            //l=a+b
+    m=PROD(a,b);                           //m=a*b
     i=SUM(4,6);
     j=PROD(4,6);
     p=SUM(2.2,3.4);
     q=PROD(3.3,3.4);
     printf("l=%d,m=%d,i=%d,j=%d,p=%f\n",l,m,i,j,p,q);
 }
+
+
+/*OUTPUT:
+l=8,m=15,i=10,j=24,p=5.600000
+*/
