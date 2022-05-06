8f1da0cf2d921b94685ea4af9c01025ffd0db42c
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-06 11:59:39 +0530

    Update problem3.c

--- 5-5-2022/problem3.c
+++ 5-5-2022/problem3.c
@@ -1,11 +1,15 @@
 //swap the value of 2 variable of any datatype
 
 #include<stdio.h>
-#define SWAP(dtype,x,y) { dtype t; t=x, x=y, y=t;}
+#define SWAP(dtype,x,y) { dtype t; t=x, x=y, y=t;}         //dtype is for data type
 
 int main()
 {
     int a=2, b=5;
-    SWAP(int,a,b);
+    SWAP(int,a,b);                                   //here change the interger a,b
     printf("a=%d,b=%d\n",a,b);
 }
+
+/*OUTPUT:
+a=5,b=2
+*/
