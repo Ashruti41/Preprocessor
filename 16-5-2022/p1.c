01db061d95f838f4f471c9a1722dffea41aeb170
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-05-16 11:43:57 +0530

    Create p1.c

---
+++ 16-5-2022/p1.c
@@ -0,0 +1,19 @@
+//with the help of function we can avoid rewriting same code again and again and we easily divided code into some parts.
+
+
+#include<stdio.h>
+int sum(int a, int b);       //function declaration
+
+int main()
+{
+    int add;
+    add=sum(2,3);              //function call
+    printf("sum=%d",add);
+}
+
+int sum(int a, int b)         //function defination
+{
+    int add=0;
+    add = a + b;
+    return add;
+}
